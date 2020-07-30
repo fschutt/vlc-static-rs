5fc40d3c483f6868afb0a8f3dcef79280a94161a
fschutt <felix.schuett@maps4print.com>
2020-07-30 16:51:37 +0200

    Adde /media_list and /media_library

--- src/lib.rs
+++ src/lib.rs
@@ -6,7 +6,11 @@ pub mod enums;
 pub mod core;
 pub mod tools;
 pub mod media;
+pub mod media_list;
+pub mod media_library;
 
 pub use crate::enums::*;
 pub use crate::core::*;
 pub use crate::media::*;
+pub use crate::media_list::*;
+pub use crate::media_library::*;
---
+++ src/media_library.rs
@@ -0,0 +1,46 @@
+// Copyright (c) 2015 T. Okubo
+// This file is part of vlc-rs.
+// Licensed under the MIT license, see the LICENSE file.
+
+use crate::sys;
+use crate::{Instance, MediaList};
+
+pub struct MediaLibrary {
+    pub(crate) ptr: *mut sys::libvlc_media_library_t,
+}
+
+impl MediaLibrary {
+    /// Create an new Media Library object.
+    pub fn new(instance: &Instance) -> Option<MediaLibrary> {
+        unsafe{
+            let p = (sys::get_vlc_dll().libvlc_media_library_new)(instance.ptr);
+            if p.is_null() { None }else{ Some(MediaLibrary{ptr: p}) }
+        }
+    }
+
+    /// Load media library.
+    pub fn load(&self) -> Result<(), ()> {
+        unsafe{
+            if (sys::get_vlc_dll().libvlc_media_library_load)(self.ptr) == 0 { Ok(()) }else{ Err(()) }
+        }
+    }
+
+    /// Get media library subitems.
+    pub fn media_list(&self) -> Option<MediaList> {
+        unsafe{
+            let p = (sys::get_vlc_dll().libvlc_media_library_media_list)(self.ptr);
+            if p.is_null() { None }else{ Some(MediaList{ptr: p}) }
+        }
+    }
+
+    /// Returns raw pointer
+    pub fn raw(&self) -> *mut sys::libvlc_media_library_t {
+        self.ptr
+    }
+}
+
+impl Drop for MediaLibrary {
+    fn drop(&mut self) {
+        unsafe{ (sys::get_vlc_dll().libvlc_media_library_release)(self.ptr) };
+    }
+}
